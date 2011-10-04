c3ea93d673fab929d216a3e0fcd4ee7e5c586df4
Luke Dashjr <luke-jr+git@utopios.org>
2011-10-03 20:27:15 -0400

    Ignore fees on transactions paying me, when deciding whether to accept them into my memory pool

--- src/main.cpp
+++ src/main.cpp
@@ -740,7 +740,7 @@ bool CTxMemPool::accept(CTransaction &tx, bool fCheckInputs,
         int64 nFees = tx.GetValueIn(view)-tx.GetValueOut();
         unsigned int nSize = ::GetSerializeSize(tx, SER_NETWORK, PROTOCOL_VERSION);
 
-        if (!fFromMe)
+        if (!fFromMe || pwalletMain->IsMine(tx))
         {
 
         // Don't accept it if it can't get into a block
