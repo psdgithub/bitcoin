2bc681e2d2ff1ebe3a508e7803d4309111d1d8ab
Luke Dashjr <luke-jr+git@utopios.org>
2011-12-05 01:42:08 -0500

    pszSubVer .eligius18

--- src/serialize.h
+++ src/serialize.h
@@ -34,7 +34,7 @@ class CAutoFile;
 static const unsigned int MAX_SIZE = 0x02000000;
 
 static const int VERSION = 32300;
-static const char* pszSubVer = ".eligius17";
+static const char* pszSubVer = ".eligius18";
 static const bool VERSION_IS_BETA = true;
 
 
