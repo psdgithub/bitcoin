3650ca821e36a67f2cdf61e65863cfaba1815a8e
Luke Dashjr <luke-jr+git@utopios.org>
2012-01-21 22:25:20 -0500

    Remove BIP 16 P2SH support

--- src/init.cpp
+++ src/init.cpp
@@ -486,25 +486,6 @@ bool AppInit2(int argc, char* argv[])
     fAllowDNS = GetBoolArg("-dns");
     fNoListen = !GetBoolArg("-listen", true);
 
-    // This code can be removed once a super-majority of the network has upgraded.
-    if (GetBoolArg("-bip16", true))
-    {
-        if (fTestNet)
-            SoftSetArg("-paytoscripthashtime", "1329264000"); // Feb 15
-        else
-            SoftSetArg("-paytoscripthashtime", "1330578000"); // Mar 1
-
-        // Put "/P2SH/" in the coinbase so everybody can tell when
-        // a majority of miners support it
-        const char* pszP2SH = "/P2SH/";
-        COINBASE_FLAGS << std::vector<unsigned char>(pszP2SH, pszP2SH+strlen(pszP2SH));
-    }
-    else
-    {
-        const char* pszP2SH = "NOP2SH";
-        COINBASE_FLAGS << std::vector<unsigned char>(pszP2SH, pszP2SH+strlen(pszP2SH));
-    }
-
     // Command-line args override in-wallet settings:
 #if USE_UPNP
     fUseUPnP = GetBoolArg("-upnp", true);
