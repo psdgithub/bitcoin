d0be9851bda9998048cba5c876784d077835013a
Philip Kaufmann <phil.kaufmann@t-online.de>
2012-04-01 14:07:29 +0200

    massive URI-handling / IPC server re-work

--- src/qt/qtipcserver.h
+++ src/qt/qtipcserver.h
@@ -1,4 +1,28 @@
-#define BITCOINURI_QUEUE_NAME "BitcoinURI"
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
 
-void ipcInit();
-void ipcShutdown();
+using namespace boost;
+
+/** define Bitcoin-Qt message queue name */
+#define BCQT_MQ_NAME "BitcoinURI"
+
+/** define Bitcoin-Qt message queue maximum message number */
+#define BCQT_MQ_MAX_MESSAGES 2
+
+/** define Bitcoin-Qt message queue maximum message size */
+#define BCQT_MQ_MAX_MESSAGE_SIZE 255
+
+enum ipcState {
+    IPC_NOT_INITIALIZED = 0,
+    IPC_INITIALIZED = 1
+};
+
+/** global state declaration */
+extern ipcState GlobalIpcState;
+
+bool ipcRemove(const char* pszFilename);
+void ipcThread(void* pArg);
+void ipcThread2(void* pArg);
+void ipcInit(bool fUseMQModeOpenOnly = true, bool fInitCalledAfterRecovery = false);
+
+#endif
