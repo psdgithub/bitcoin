29eb4c3e51506c3e0617fef63966f0e47837e140
Giel van Schijndel <me@mortis.eu>
2012-07-08 23:22:08 +0200

    Give the GUI-IPC thread a name as well
    Signed-off-by: Giel van Schijndel <me@mortis.eu>

--- src/qt/qtipcserver.cpp
+++ src/qt/qtipcserver.cpp
@@ -14,6 +14,7 @@
 
 #include "ui_interface.h"
 #include "qtipcserver.h"
+#include "util.h"
 
 using namespace boost::interprocess;
 using namespace boost::posix_time;
@@ -27,6 +28,9 @@ void ipcShutdown()
 
 void ipcThread(void* parg)
 {
+    // Make this thread recognisable as the GUI-IPC thread
+    RenameThread("bitcoin-gui-ipc");
+
     message_queue* mq = (message_queue*)parg;
     char strBuf[257];
     size_t nSize;
