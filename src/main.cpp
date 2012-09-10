39791374187a86a61b3170536b9d9aa359b5557d
Luke Dashjr <luke-jr+git@utopios.org>
2012-09-10 02:20:04 +0000

    submitblock RPC: Return "orphan" for blocks we don't have the parent of, but still save them

--- src/main.cpp
+++ src/main.cpp
@@ -2434,7 +2434,8 @@ bool ProcessBlock(CValidationState &state, CNode* pfrom, CBlock* pblock, CDiskBl
             // Ask this guy to fill in what we're missing
             PushGetBlocks(pfrom, chainActive.Tip(), GetOrphanRoot(hash));
         }
-        return true;
+        // The block is accepted, but not immediately processed
+        return state.Orphan();
     }
 
     // Store to disk
