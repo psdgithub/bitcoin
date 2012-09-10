6707a08edb3706c71c00c4a7b3b843e3cbce9e79
Luke Dashjr <luke-jr+git@utopios.org>
2012-09-10 02:55:03 +0000

    Implement BIP 23 Block Proposal

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -399,6 +399,8 @@ Value getwork(const Array& params, bool fHelp)
 }
 #endif
 
+static Value TestBlock(const Value& valData, bool fCheckPOW);
+
 Value getblocktemplate(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() > 1)
@@ -474,6 +476,15 @@ Value getblocktemplate(const Array& params, bool fHelp)
         }
         else
             throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid mode");
+
+        if (strMode == "proposal")
+        {
+            const Value& dataval = find_value(oparam, "data");
+            if (dataval.type() == str_type)
+                return TestBlock(dataval, false);
+            else
+                throw JSONRPCError(RPC_TYPE_ERROR, "Missing data String key for proposal");
+        }
     }
 
     if (strMode != "template")
@@ -520,6 +531,10 @@ Value getblocktemplate(const Array& params, bool fHelp)
     // Update nTime
     UpdateTime(*pblock, pindexPrev);
     pblock->nNonce = 0;
+
+    static Array aCaps;
+    if (aCaps.empty())
+        aCaps.push_back("proposal");
 
     Array transactions;
     map<uint256, int64_t> setTxIndex;
@@ -569,6 +584,7 @@ Value getblocktemplate(const Array& params, bool fHelp)
     }
 
     Object result;
+    result.push_back(Pair("capabilities", aCaps));
     result.push_back(Pair("version", pblock->nVersion));
     result.push_back(Pair("previousblockhash", pblock->hashPrevBlock.GetHex()));
     result.push_back(Pair("transactions", transactions));
@@ -585,6 +601,42 @@ Value getblocktemplate(const Array& params, bool fHelp)
     result.push_back(Pair("height", (int64_t)(pindexPrev->nHeight+1)));
 
     return result;
+}
+
+static Value TestBlock(const Value& valData, bool fCheckPOW)
+{
+    vector<unsigned char> blockData(ParseHex(valData.get_str()));
+    CDataStream ssBlock(blockData, SER_NETWORK, PROTOCOL_VERSION);
+    CBlock pblock;
+    try {
+        ssBlock >> pblock;
+    }
+    catch (std::exception &e) {
+        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Block decode failed");
+    }
+
+    CValidationState state;
+    bool fAccepted = ProcessBlock(state, NULL, &pblock, NULL, fCheckPOW);
+    if (!fAccepted)
+    {
+        std::string strRejectReason = state.GetRejectReason();
+        if (state.IsError())
+            throw JSONRPCError(RPC_MISC_ERROR, strRejectReason);
+        if (state.IsInvalid())
+        {
+            if (strRejectReason.empty())
+                return "rejected";
+            return strRejectReason;
+        }
+        // Should be impossible
+        return "valid?";
+    }
+
+    // NOTE: If we process an orphan, it is accepted yet not immediately processed
+    if (state.IsOrphan())
+        return "orphan";
+
+    return Value::null;
 }
 
 Value submitblock(const Array& params, bool fHelp)
@@ -608,36 +660,5 @@ Value submitblock(const Array& params, bool fHelp)
             + HelpExampleRpc("submitblock", "\"mydata\"")
         );
 
-    vector<unsigned char> blockData(ParseHex(params[0].get_str()));
-    CDataStream ssBlock(blockData, SER_NETWORK, PROTOCOL_VERSION);
-    CBlock pblock;
-    try {
-        ssBlock >> pblock;
-    }
-    catch (std::exception &e) {
-        throw JSONRPCError(RPC_DESERIALIZATION_ERROR, "Block decode failed");
-    }
-
-    CValidationState state;
-    bool fAccepted = ProcessBlock(state, NULL, &pblock);
-    if (!fAccepted)
-    {
-        std::string strRejectReason = state.GetRejectReason();
-        if (state.IsError())
-            throw JSONRPCError(RPC_MISC_ERROR, strRejectReason);
-        if (state.IsInvalid())
-        {
-            if (strRejectReason.empty())
-                return "rejected";
-            return strRejectReason;
-        }
-        // Should be impossible
-        return "valid?";
-    }
-
-    // NOTE: If we process an orphan, it is accepted yet not immediately processed
-    if (state.IsOrphan())
-        return "orphan";
-
-    return Value::null;
+    return TestBlock(params[0], true);
 }
