28f8035413cffe793710870d38fe8e07503dd271
Luke Dashjr <luke-jr+git@utopios.org>
2012-11-25 21:40:40 +0000

    Append user agent with /next-test:20121125/

--- src/util.cpp
+++ src/util.cpp
@@ -1294,7 +1294,7 @@ std::string FormatSubVersion(const std::string& name, int nClientVersion, const 
     if (!comments.empty())
         ss << "(" << boost::algorithm::join(comments, "; ") << ")";
     ss << "/";
-    ss << "next" << ":" << "20121125";
+    ss << "next-test" << ":" << "20121125";
     ss << "/";
     return ss.str();
 }
