51d5f22119ea923f5074594b4bfa11893f092289
Gavin Andresen <gavinandresen@gmail.com>
2013-07-15 15:20:50 +1000

    Refactor: CAddressBookData for mapAddressBook
    Straight refactor, so mapAddressBook stores a CAddressBookData
    (which just contains a std::string) instead of a std::string.
    
    Preparation for payment protocol work, which will add the notion
    of refund addresses to the address book.

--- src/walletdb.cpp
+++ src/walletdb.cpp
@@ -211,7 +211,7 @@ ReadKeyValue(CWallet* pwallet, CDataStream& ssKey, CDataStream& ssValue,
         {
             string strAddress;
             ssKey >> strAddress;
-            ssValue >> pwallet->mapAddressBook[CBitcoinAddress(strAddress).Get()];
+            ssValue >> pwallet->mapAddressBook[CBitcoinAddress(strAddress).Get()].name;
         }
         else if (strType == "tx")
         {
