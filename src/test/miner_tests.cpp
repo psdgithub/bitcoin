tmagik/catoshi

Fix minor unit test memory leaks

src/test/miner_tests.cpp
@@ -83,6 +83,7 @@ BOOST_AUTO_TEST_CASE(CreateNewBlock_validity)
 
     // Just to make sure we can still make simple blocks
     BOOST_CHECK(pblocktemplate = CreateNewBlockWithKey(reservekey));
+    delete pblocktemplate;
 
     // block sigops > limit: 1000 CHECKMULTISIG + 1
     tx.vin.resize(1);
@@ -200,6 +201,9 @@ BOOST_AUTO_TEST_CASE(CreateNewBlock_validity)
     BOOST_CHECK(pblocktemplate = CreateNewBlockWithKey(reservekey));
     delete pblocktemplate;
     pindexBest->nHeight = nHeight;
+
+    BOOST_FOREACH(CTransaction *tx, txFirst)
+        delete tx;
 }
 
 BOOST_AUTO_TEST_CASE(sha256transform_equality)

psdgithub/bitcoin

src/test/miner_tests.cpp
@@ -78,6 +78,7 @@ BOOST_AUTO_TEST_CASE(CreateNewBlock_validity)
 
     // Just to make sure we can still make simple blocks
     BOOST_CHECK(pblock = CreateNewBlock(reservekey));
+    delete pblock;
 
     // block sigops > limit: 1000 CHECKMULTISIG + 1
     tx.vin.resize(1);
@@ -195,6 +196,9 @@ BOOST_AUTO_TEST_CASE(CreateNewBlock_validity)
     BOOST_CHECK(pblock = CreateNewBlock(reservekey));
     delete pblock;
     pindexBest->nHeight = nHeight;
+
+    BOOST_FOREACH(CTransaction *tx, txFirst)
+        delete tx;
 }
 
 BOOST_AUTO_TEST_CASE(sha256transform_equality)