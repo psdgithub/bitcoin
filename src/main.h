9e7535d40d01d3a2b7fc137d63b169e1c59d6fc4
Ruben Dario Ponticelli <rdponticelli@gmail.com>
2014-07-08 09:50:35 -0300

    Add a switch to allow running in a pruned state.
    These are the main functional changes on this state:
    
    * Do not allow running with a wallet or txindex.
    * Check for data at startup is mandatory only up to the last 288 blocks.
    * NODE_NETWORK flag is unset.
    * Requests for pruned blocks from other peers is answered with "notfound" and they are disconnected, not to stall their IBD.

--- src/main.h
+++ src/main.h
@@ -96,6 +96,8 @@ static const unsigned int BLOCK_DOWNLOAD_WINDOW = 1024;
 static const unsigned int DATABASE_WRITE_INTERVAL = 3600;
 /** Maximum length of reject messages. */
 static const unsigned int MAX_REJECT_MESSAGE_LENGTH = 111;
+/** Minimum amount of blocks to keep unpruned, needed to afford deep reorganizations. */
+static const unsigned int MIN_BLOCKS_TO_KEEP = 288;
 
 /** "reject" message codes */
 static const unsigned char REJECT_MALFORMED = 0x01;
@@ -130,6 +132,7 @@ extern bool fTxIndex;
 extern bool fIsBareMultisigStd;
 extern unsigned int nCoinCacheSize;
 extern CFeeRate minRelayTxFee;
+extern bool fPruned;
 
 /** Best header we've seen so far (used for getheaders queries' starting points). */
 extern CBlockIndex *pindexBestHeader;
