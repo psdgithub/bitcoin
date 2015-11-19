4baa4fa8d977f115bd33ef64d9c8443baed62afe
Daniel Cousens <github@dcousens.com>
2015-11-19 13:27:18 +1100

    zmq: prepend zmq to debug messages
    Github-Pull: #7058
    Rebased-From: ffacd27def73834bc15706191c2ed9cb99f565a6

--- src/zmq/zmqpublishnotifier.cpp
+++ src/zmq/zmqpublishnotifier.cpp
@@ -77,7 +77,7 @@ bool CZMQAbstractPublishNotifier::Initialize(void *pcontext)
     }
     else
     {
-        LogPrint("zmq", "  Reuse socket for address %s\n", address);
+        LogPrint("zmq", "zmq: Reusing socket for address %s\n", address);
 
         psocket = i->second->psocket;
         mapPublishNotifiers.insert(std::make_pair(address, this));
@@ -118,7 +118,7 @@ void CZMQAbstractPublishNotifier::Shutdown()
 
 bool CZMQPublishHashBlockNotifier::NotifyBlock(const uint256 &hash)
 {
-    LogPrint("zmq", "Publish hash block %s\n", hash.GetHex());
+    LogPrint("zmq", "zmq: Publish hashblock %s\n", hash.GetHex());
     char data[32];
     for (unsigned int i = 0; i < 32; i++)
         data[31 - i] = hash.begin()[i];
@@ -129,7 +129,7 @@ bool CZMQPublishHashBlockNotifier::NotifyBlock(const uint256 &hash)
 bool CZMQPublishHashTransactionNotifier::NotifyTransaction(const CTransaction &transaction)
 {
     uint256 hash = transaction.GetHash();
-    LogPrint("zmq", "Publish hash transaction %s\n", hash.GetHex());
+    LogPrint("zmq", "zmq: Publish hashtx %s\n", hash.GetHex());
     char data[32];
     for (unsigned int i = 0; i < 32; i++)
         data[31 - i] = hash.begin()[i];
@@ -139,7 +139,7 @@ bool CZMQPublishHashTransactionNotifier::NotifyTransaction(const CTransaction &t
 
 bool CZMQPublishRawBlockNotifier::NotifyBlock(const uint256 &hash)
 {
-    LogPrint("zmq", "Publish raw block %s\n", hash.GetHex());
+    LogPrint("zmq", "zmq: Publish rawblock %s\n", hash.GetHex());
 
     CDataStream ss(SER_NETWORK, PROTOCOL_VERSION);
     {
@@ -164,7 +164,7 @@ bool CZMQPublishRawBlockNotifier::NotifyBlock(const uint256 &hash)
 bool CZMQPublishRawTransactionNotifier::NotifyTransaction(const CTransaction &transaction)
 {
     uint256 hash = transaction.GetHash();
-    LogPrint("zmq", "Publish raw transaction %s\n", hash.GetHex());
+    LogPrint("zmq", "zmq: Publish rawtx %s\n", hash.GetHex());
     CDataStream ss(SER_NETWORK, PROTOCOL_VERSION);
     ss << transaction;
     int rc = zmq_send_multipart(psocket, "rawtx", 5, &(*ss.begin()), ss.size(), 0);
