78bcf177feec4a27c2edc5e8ca4187e8a62abe6f
Luke Dashjr <luke-jr+git@utopios.org>
2016-01-16 23:49:42 +0000

    AreInputsStandard: Return specific reject reasons

--- src/test/test_bitcoin.h
+++ src/test/test_bitcoin.h
@@ -7,6 +7,7 @@
 
 #include "chainparamsbase.h"
 #include "key.h"
+#include "policy/policy.h"
 #include "pubkey.h"
 #include "txdb.h"
 
@@ -87,4 +88,10 @@ struct TestMemPoolEntryHelper
     TestMemPoolEntryHelper &SpendsCoinbase(bool _flag) { spendsCoinbase = _flag; return *this; }
     TestMemPoolEntryHelper &SigOps(unsigned int _sigops) { sigOpCount = _sigops; return *this; }
 };
+
+static inline bool AreInputsStandard(const CTransaction& tx, const CCoinsViewCache& mapInputs) {
+    std::string reason;
+    return AreInputsStandard(tx, mapInputs, reason);
+}
+
 #endif
